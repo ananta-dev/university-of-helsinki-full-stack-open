aa5da7d74f3b915a44b9e2824262621ce82b95d6
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-02-13 20:23:31 +0000

    Exercise 1.6 finished

--- part01/src/App.js
+++ part01/src/App.js
@@ -1,123 +1,35 @@
 import { useState } from "react";
-const Header = props => <h1>{props.courseName}</h1>;
-
-const Part = props => (
-    <p>
-        {props.name} {props.exercises}
-    </p>
-);
-
-const Content = props => (
-    <>
-        {props.courseParts.map(part => (
-            <Part key={part.id} name={part.name} exercises={part.exercises} />
-        ))}
-    </>
-);
-
-const Total = props => {
-    // --- A more naive way: ---
-    // let totalNumber = 0;
-    // props.parts.forEach(part => (totalNumber += part.exercises));
-
-    // --- A more ES6 way with reduce ---
-    const totalNumber = props.courseParts.reduce((x, y) => ({
-        exercises: x.exercises + y.exercises,
-    })).exercises;
-
-    return <p>Number of exercises {totalNumber}</p>;
-};
-
-const Display = ({ counter }) => {
-    return <div>{counter}</div>;
-};
-
-const Button = ({ onClickFunction, text }) => {
-    return <button onClick={onClickFunction}>{text}</button>;
-};
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            {
-                id: 1,
-                name: "Fundamentals of React",
-                exercises: 10,
-            },
-            {
-                id: 2,
-                name: "Using props to pass data",
-                exercises: 7,
-            },
-            {
-                id: 3,
-                name: "State of a component",
-                exercises: 14,
-            },
-        ],
-    };
-    const [value, setValue] = useState(10);
-    const [valueB, setValueB] = useState(10);
-
-    // setTimeout(() => setCounter(counter + 1), 2000);
-
-    const increaseByOne = () => setCounter(counter + 1);
-    const decreaseByOne = () => setCounter(counter - 1);
-    const setToZero = () => setCounter(0);
-    const increaseLeft = () => setLeft(left + 1);
-    const increaseRight = () => setRight(right + 1);
-    const setToValue = newValue => () => {
-        console.log("value now", newValue); // print the new value to console
-        setValue(newValue);
+    const increaseGood = () => {
+        setGood(good + 1);
     };
 
-    const setToValueB = newValue => {
-        console.log("value now", newValue);
-        setValueB(newValue);
+    const increaseNeutral = () => {
+        setNeutral(neutral + 1);
     };
 
+    const increaseBad = () => {
+        setBad(bad + 1);
+    };
     return (
         <div>
-            <Header courseName={course.name} />
-            <Content courseParts={course.parts} />
-            <Total courseParts={course.parts} />
-            <Display counter={counter} />
-            <Button onClickFunction={increaseByOne} text='Increase' />
-            <Button onClickFunction={decreaseByOne} text='Decrease' />
-            <Button onClickFunction={setToZero} text='Reset' />
+            <h1>give feedback</h1>
+            <button onClick={increaseGood}>good</button>
+            <button onClick={increaseNeutral}>neutral</button>
+            <button onClick={increaseBad}>bad</button>
+            <h1>statistics</h1>
+            <strong>good {good}</strong>
             <br />
-            <strong>Left: </strong>
-            <Display counter={left} />
-            <strong>Right: </strong>
-            <Display counter={right} />
-            <Button onClickFunction={increaseLeft} text='Left' />
-            <Button onClickFunction={increaseRight} text='Right' />
+            <strong>neutral {neutral}</strong>
             <br />
+            <strong>bad {bad}</strong>
             <br />
-            <div>
-                {value}
-                <br />
-                <br />
-                <button onClick={setToValue(1000)}>thousand</button>
-                <button onClick={setToValue(0)}>reset</button>
-                <button onClick={setToValue(value + 1)}>increment</button>
-            </div>
-            <br />
-            <br />
-            <div>
-                {valueB}
-                <br />
-                <button onClick={() => setToValueB(1000)}>thousand (B)</button>
-                <button onClick={() => setToValueB(0)}>reset (B)</button>
-                <button onClick={() => setToValueB(value + 1)}>
-                    increment (B)
-                </button>
-            </div>
         </div>
     );
 };
