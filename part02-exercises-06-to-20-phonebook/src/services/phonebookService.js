59583b796222676d09ae4de6e86281491d0f43f1
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-09-12 07:45:21 +0100

    Working on module 4 code along

--- part02-exercises-06-to-20-phonebook/src/services/phonebookService.js
+++ part02-exercises-06-to-20-phonebook/src/services/phonebookService.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+// const baseUrl = "http://localhost:3001/persons";
+
+// This frontend is going to be deployed inside the backend, so we use a relative path
+// If we deployed the frontend to a different server we would have to use the following:
+// const baseUrl = "https://ancient-everglades-91112.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
     return axios.get(baseUrl);
